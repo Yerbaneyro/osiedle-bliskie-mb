b8f2c02f7aa18da5c5b8a2b9fea94516a356e66d
Marcin Bednarz <94953520+Yerbaneyro@users.noreply.github.com>
2022-03-04 11:29:15 +0100

    Navbar and GlobalFonts
    Navbar and GlobalFonts implemented

---
+++ src/globalStyles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+import "@fontsource/work-sans";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: "Work Sans" ;
+    }
+`;
+
+export default GlobalStyle;
